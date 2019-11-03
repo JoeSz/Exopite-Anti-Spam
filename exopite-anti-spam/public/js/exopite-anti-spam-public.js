f42bf163b7e234a2b5c7111d6b2aeeafb7cfd25c
JoeSz <ujbeszel@gmail.com>
2019-11-03 19:37:14 +0100

    Add AJAX loading for CF7 and fix wpcf7mailsent and wpcf7invalid hooks (apply only for this instance)

--- exopite-anti-spam/public/js/exopite-anti-spam-public.js
+++ exopite-anti-spam/public/js/exopite-anti-spam-public.js
@@ -3,10 +3,115 @@
 
     "use strict";
 
+    var pluginName = "exopiteAntiSpamAjax",
+        defaults = {};
+
+    function Plugin(element, options) {
+        this.element = element;
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    $.extend(Plugin.prototype, {
+        init: function () {
+            var plugin = this;
+            plugin.buildCache();
+
+            plugin.ajaxUrl = plugin.$element.data('ajax-url');
+
+            if (!plugin.ajaxUrl.length) {
+                return;
+            }
+
+            plugin.bindEvents();
+        },
+        bindEvents: function () {
+            var plugin = this;
+
+            plugin.$element.find('.eas-cf7-shortcode-load').off().on('click' + '.' + plugin._name, function (e) {
+                plugin.ajaxLoad.call(plugin, e);
+            });
+
+        },
+        destroy: function () {
+            this.unbindEvents();
+            this.$element.removeData();
+        },
+        unbindEvents: function () {
+            this.$element.off('.' + this._name);
+        },
+        buildCache: function () {
+            this.$element = $(this.element);
+        },
+        setElements: function (plugin, response) {
+            plugin.$element.html(response);
+            var $form = plugin.$element.find('.wpcf7-form');
+            var action = $form.attr('action').split("#");
+            $form.attr('action', '#' + action[1]);
+
+            plugin.$element.trigger("easSetElementsBefore", [plugin, response]);
+
+            /**
+             * @link https://wordpress.org/support/topic/init-function-wpcf7initform/
+             */
+            var $form = plugin.$element.find('.wpcf7 > form');
+
+            wpcf7.initForm( $form );
+            if ( wpcf7.cached ) {
+               wpcf7.refill( $form );
+            }
+            $form.exopiteAntiSpam();
+
+            plugin.$element.trigger("easSetElementsAfter", [plugin, response]);
+
+        },
+        ajaxLoad: function (e) {
+            var plugin = this;
+            e.preventDefault();
+
+            var dataJSON = {
+                'action': 'eas_get_contact_form_7_ajax',
+                'cf7_id': plugin.$element.data('id'),
+                'cf7_title': plugin.$element.data('title'),
+                'eas_cf7_ajax': true,
+            };
+
+            $.ajax({
+                cache: false,
+                type: "POST",
+                url: plugin.ajaxUrl,
+                data: dataJSON,
+                success: function( response ){
+                    plugin.setElements(plugin, response);
+                },
+                error: function( xhr, status, error ) {
+                    console.log( 'Status: ' + xhr.status );
+                    console.log( 'Error: ' + xhr.responseText );
+                },
+            });
+
+        },
+    });
+
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+            }
+        });
+    };
+
+})(jQuery, window, document);
+
+
+; (function ($, window, document, undefined) {
+
+    "use strict";
+
     var pluginName = "exopiteAntiSpam",
-        defaults = {
-            propertyName: "value"
-        };
+        defaults = {};
 
     function Plugin(element, options) {
         this.element = element;
@@ -41,14 +146,14 @@
         },
         bindEvents: function () {
             var plugin = this;
-            $(document).on('wpcf7mailsent', function () {
-                plugin.getElements(true,true);
+
+            plugin.$element.parents('.wpcf7').on('wpcf7mailsent', function (e) {
+                plugin.getElements(true, true);
             });
-            $(document).on('wpcf7invalid', function () {
+
+            plugin.$element.parents('.wpcf7').on('wpcf7invalid', function (e) {
                 plugin.getElements(false, true);
             });
-
-
         },
         destroy: function () {
             this.unbindEvents();
@@ -79,6 +184,7 @@
                 'action': 'eap_reload_cf7_fields',
                 'exanspselAuth': exanspselAuth,
                 'timestamp': timestamp,
+                'eas_cf7_ajax': true,
                 // 'nonce': 'nonce-key',
             };
 
@@ -133,7 +239,35 @@
 
     $(function() {
 
+        $('.eas-cf7-shortcode').exopiteAntiSpamAjax();
         $('.wpcf7-form').exopiteAntiSpam();
+
+        $('.eas-cf7-shortcode').on('easSetElementsAfter', function (event, plugin, response){;
+            var TextOJB = dnd_cf7_uploader.drag_n_drop_upload;
+            plugin.$element.find('.wpcf7-drag-n-drop-file').CodeDropz_Uploader({
+                'color'				:	'#fff',
+                'ajax_url'			: 	dnd_cf7_uploader.ajax_url,
+                'text'				: 	TextOJB.text,
+                'separator'			: 	TextOJB.or_separator,
+                'button_text'		:	TextOJB.browse,
+                'server_max_error'	: 	TextOJB.server_max_error,
+                'on_success'		:	function( input, progressBar, response ){
+
+                    // Progressbar Object
+                    var progressDetails = $('#' + progressBar, input.parents('.codedropz-upload-wrapper') );
+
+                    // If it's complete remove disabled attribute in button
+                    if( $('.in-progress', input.parents('form') ).length === 0 ) {
+                        setTimeout(function(){ $('input[type="submit"]', input.parents('form')).removeAttr('disabled'); }, 1);
+                    }
+
+                    // Append hidden input field
+                    progressDetails
+                        .find('.dnd-upload-details')
+                            .append('<span><input type="hidden" name="'+ input.attr('data-name') +'[]" value="'+ response.data.path +'/'+ response.data.file +'"></span>');
+                }
+            });
+        });
 
     });
 
