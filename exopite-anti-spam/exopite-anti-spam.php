68f26f5df83540fe23ef5765482ceac60ae5ba64
JoeSz <ujbeszel@gmail.com>
2019-11-10 17:53:11 +0100

    Add basic plugin infos

--- exopite-anti-spam/exopite-anti-spam.php
+++ exopite-anti-spam/exopite-anti-spam.php
@@ -15,8 +15,8 @@
  * @wordpress-plugin
  * Plugin Name:       Exopite Anti Spam
  * Plugin URI:        https://www.joeszalai.org/exopite/anti-spam
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Description:       Anti Spam plugin with timestamp, honeypot (random location), token matching, bad/spam word filtering, email and domain blacklist and an image captcha.
+ * Version:           20191104
  * Author:            Joe Szalai
  * Author URI:        https://www.joeszalai.org
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'EXOPITE_ANTI_SPAM_VERSION', '1.0.0' );
+define( 'EXOPITE_ANTI_SPAM_VERSION', '20191104' );
 define( 'EXOPITE_ANTI_SPAM_URL', plugin_dir_url( __FILE__ ) );
 define( 'EXOPITE_ANTI_SPAM_PATH', plugin_dir_path( __FILE__ ) );
 define( 'EXOPITE_ANTI_SPAM_FILE', __FILE__ );
